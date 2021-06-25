f2f8407b9cd3fe58f6a3805081ec79aee9932785
jwolfe972 <jordanwolfe2@my.untdallas.edu>
2021-06-25 14:58:38 -0500

    Hello You App

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+
+
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Hello App",
+      theme: new ThemeData(
+        primarySwatch: Colors.blueGrey
+
+      ),
+
+
+      home: MyHome(),
+    );
+
+
+
+    throw UnimplementedError();
+  }
+
+
+
+
+}
+
+class MyHome  extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _HelloYouState();
+
+
+
+
+    // throw UnimplementedError();
+
+
+
+}
+
+class _HelloYouState  extends State<MyHome>{
+
+  String name = "";
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello You App"),
+        backgroundColor: Colors.pinkAccent,
+
+      ),
+
+      body: Container(
+
+        padding: EdgeInsets.all(15.0),
+        child: Column(children: <Widget> [
+          TextField(
+
+            onChanged: (String s){
+
+              setState(() {
+                name = s;
+              });
+            },
+
+          ),
+          Text('Hello ' + name + '!'),
+
+
+
+        ],),
+      ),
+    );
+
+
+
+
+
+
+
+    throw UnimplementedError();
+  }
+}
+
